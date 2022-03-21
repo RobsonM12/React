f28d69734a266b4149f790ab94afc40468b5ae98
RobsonM12 <robsonmorais1996@hotmail.com>
2022-03-21 13:58:23 -0300

    React_template

---
+++ src/paginas/Home/home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./home.css";
+
+function Home() {
+  return (
+    <>
+    <h1 className='titulo'>Home</h1>
+    <p><img src="http://g1.globo.com/Noticias/Ciencia/foto/0,,14316948,00.jpg " alt="imagem tela inicial" className='img'  /></p>
+    </>
+  )
+}
+
+export default Home;
